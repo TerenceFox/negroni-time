d3e8d72b4b740af83bc17a6792fc87b375c1d8a1
Terence Fox <terence.fo@gmail.com>
2020-06-06 14:24:54 -0400

    Background SVG animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,16 @@ import Footer from "../components/Footer"
 import Div100vh from "react-div-100vh"
 import { graphql } from "gatsby"
 import "../styles/main.scss"
-import Blob from "../../static/blob.svg"
+import blob from "../assets/blob1.svg"
 
 const App = ({ data }) => {
   const { businessDetails } = data.site.siteMetadata
   return (
-    <div>
+    <div className="wrapper" style={{ backgroundImage: `url(${blob})` }}>
       <Header businessDetails={businessDetails} />
       <Div100vh className="container">
-        <Main name={businessDetails.name}/>
+        <Main name={businessDetails.name} />
       </Div100vh>
-      <Blob className="blob" />
       <Footer businessDetails={businessDetails} />
     </div>
   )
